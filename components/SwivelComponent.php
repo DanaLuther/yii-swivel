b7d63bfa28136bc20f4715148d6ac821d1219fef
Dana Luther <dana.luther@gmail.com>
2016-03-18 13:09:31 -0400

    Added persistent bucket state for guest users during a single session. Modified phpdoc and added logging for the base user bucket value access

--- components/SwivelComponent.php
+++ components/SwivelComponent.php
@@ -18,32 +18,72 @@
  */
 class SwivelComponent extends CApplicationComponent {
 
-	/** @var SwivelLoader */
+	/**
+	 * @var SwivelLoader
+	 */
 	protected $loader;
-	/** @var array Options to be passed to the Config */
+	/**
+	 * @var array Options to be passed to the Config
+	 */
 	public $options = [];
-	
-	/** @var bool Whether to create the swivel table automatically when it does not exist */
+
+	/**
+	 * @var bool Whether to create the swivel table automatically when it does not exist
+	 */
 	public $autoCreateSwivelTable = true;
-	/** @var string The table name to be used to store the swivel features and associated buckets */
+	/**
+	 * @var string The table name to be used to store the swivel features and associated buckets
+	 */
 	public $swivelTableAlias = 'swivel';
-	/** @var string The Application component ID for the swivel database connection */
+	/**
+	 * @var string The Application component ID for the swivel database connection
+	 */
 	public $dbComponent = 'db';
 
-	/** @var string The class name for the model holding the swivel map data  */
+	/**
+	 * @var string The class name for the model holding the swivel map data
+	 */
 	public $modelClass = 'SwivelFeature';
 
-	/** @var string The name of the vendor directory where swivel is installed */
+	/**
+	 * @var string The name of the vendor directory where swivel is installed
+	 */
 	public $vendorDir = 'vendor';
-	/** @var string The alias for the location of this extension */
+	/**
+	 * @var string The alias for the location of this extension
+	 */
 	public $extensionAlias = 'application.vendor.dhluther.yii-swivel';
 
-	/** @var string The default Cookie to store the swivel bucket information for the user */
+	/**
+	 * @var string The default Cookie to store the swivel bucket information for the user
+	 */
 	public $cookieName = 'Swivel_Bucket';
-	/** @var string The name of the property on the application user model that holds their assigned bucket identifier */
-	public $userBucketProperty = 'bucketIndex';
-	/** @var null|int The default bucket ID - if null, one will be randomly generated and assigned */
+	/**
+	 * The name of the property on the application user model that holds their assigned bucket identifier
+	 * If set to null, no property on the web user model will be examined
+	 * @var null|string
+	 */
+	public $userBucketProperty = null;
+	/**
+	 * @var null|int The default bucket ID - if null, one will be randomly generated and assigned
+	 */
 	public $bucketIndex = null;
+
+	/**
+	 * This can be any callable - defaults to mt_rand(1,10)
+	 *
+	 * @var callable The function or method to be called to generate the bucket index
+	 */
+	public $generatorCallable = "mt_rand";
+	/**
+	 * @var array Arguments to be passed to the generator
+	 */
+	public $generatorArgs = [1,10];
+
+	/**
+	 * @var \Psr\Log\LoggerInterface
+	 */
+	protected $_logger;
 
 	public function init()
 	{
@@ -65,9 +105,15 @@ class SwivelComponent extends CApplicationComponent {
 		}
 
 		// If we have a registered user, assume they should have some property or magic method that holds their bucket ID
-		if ( !Yii::app()->user->isGuest )
+		if ( $this->userBucketProperty )
 		{
 			$this->bucketIndex = Yii::app()->user->{$this->userBucketProperty};
+		}
+		// If no bucket has been selected, establish a cookie for the bucket identifier and use that for the duration
+		// of the user's time on site - otherwise they will get a different bucket with each page load
+		if ( !$this->bucketIndex )
+		{
+			$this->bucketIndex = $this->checkAndApplyIndex();
 		}
 
 		$this->loader = new SwivelLoader( CMap::mergeArray( $this->getDefaultOptions(), $this->options ));
@@ -130,16 +176,41 @@ class SwivelComponent extends CApplicationComponent {
 			],
 			'BucketIndex' => $this->bucketIndex,
 			'LoaderAlias' => 'SwivelLoader',
-			'Logger' => $this->getDefaultLogger(),
+			'Logger' => $this->getLogger(),
 			'Metrics' => null,
 			'ModelAlias' => $this->modelClass,
 		];
 	}
 
 	/**
+	 * Check the user state for a bucket index, and if set, return it.
+	 * @return int
+	 */
+	protected function checkAndApplyIndex()
+	{
+		if ( !Yii::app()->user->hasState( $this->cookieName ))
+		{
+			Yii::app()->user->setState( $this->cookieName,  $this->defaultBucketGenerator() );
+			$this->getLogger()->debug( 'Set default bucket value for new user.', [ 'Bucket ID' => Yii::app()->user->getState( $this->cookieName ) ] );
+		}
+		return Yii::app()->user->getState( $this->cookieName );
+	}
+
+	/**
+	 * Generate the random bucket for the user
+	 * Override as necessary if updating the configuration options is not sufficient
+	 *
+	 * @return int
+	 */
+	public function defaultBucketGenerator()
+	{
+		return call_user_func_array( $this->generatorCallable, $this->generatorArgs );
+	}
+
+	/**
 	 * Default Log option -- can be overridden by passing a different logger through the config, or by extending this
 	 * class and overriding the method
-	 * 
+	 *
 	 * @return SwivelLogger
 	 */
 	protected function getDefaultLogger()
@@ -160,5 +231,25 @@ class SwivelComponent extends CApplicationComponent {
 				'INDEX ix_slug( slug(8) )',
 			]
 		);
+	}
+
+	/**
+	 * @param \Psr\Log\LoggerInterface $logger
+	 */
+	public function setLogger( \Psr\Log\LoggerInterface $logger )
+	{
+		$this->_logger = $logger;
+	}
+
+	/**
+	 * @return \Psr\Log\LoggerInterface
+	 */
+	public function getLogger()
+	{
+		if ( !$this->_logger )
+		{
+			$this->_logger = $this->getDefaultLogger();
+		}
+		return $this->_logger;
 	}
 }
